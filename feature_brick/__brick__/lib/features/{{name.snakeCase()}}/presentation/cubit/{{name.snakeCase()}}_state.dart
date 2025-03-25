3ac81dd569dbdef7c31878b8c6e6fae836b47b68
Cesar Posada <cesarleonardo537@gmail.com>
2025-03-24 22:04:59 -0600

    Rename state classes for consistency in {{name.pascalCase()}} feature

--- feature_brick/__brick__/lib/features/{{name.snakeCase()}}/presentation/cubit/{{name.snakeCase()}}_state.dart
+++ feature_brick/__brick__/lib/features/{{name.snakeCase()}}/presentation/cubit/{{name.snakeCase()}}_state.dart
@@ -5,17 +5,17 @@ abstract class {{name.pascalCase()}}State {
   const {{name.pascalCase()}}State();
 }
 
-class {{name.pascalCase()}}Initial extends {{name.pascalCase()}}State {}
+class {{name.pascalCase()}}StateInitial extends {{name.pascalCase()}}State {}
 
-class {{name.pascalCase()}}Loading extends {{name.pascalCase()}}State {}
+class {{name.pascalCase()}}StateLoading extends {{name.pascalCase()}}State {}
 
-class {{name.pascalCase()}}Loaded extends {{name.pascalCase()}}State {
+class {{name.pascalCase()}}StateLoaded extends {{name.pascalCase()}}State {
   final List<{{name.pascalCase()}}Entity> {{name.camelCase()}};
-  const {{name.pascalCase()}}Loaded(this.{{name.camelCase()}});
+  const {{name.pascalCase()}}StateLoaded(this.{{name.camelCase()}});
 }
 
-class {{name.pascalCase()}}Error extends {{name.pascalCase()}}State {
+class {{name.pascalCase()}}StateError extends {{name.pascalCase()}}State {
   final String message;
   
-  const {{name.pascalCase()}}Error(this.message);
+  const {{name.pascalCase()}}StateError(this.message);
 }
