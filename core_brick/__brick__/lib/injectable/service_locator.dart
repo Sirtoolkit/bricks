a7c1d127994a8ea1c761b30d2d1740ac6a3b6559
Cesar Posada <cesarleonardo537@gmail.com>
2025-03-24 21:50:36 -0600

    Update import path for service_locator.config.dart in service_locator.dart

--- core_brick/__brick__/lib/injectable/service_locator.dart
+++ core_brick/__brick__/lib/injectable/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'package:salonista_app/injectable/service_locator.config.dart';
+
+import 'service_locator.config.dart';
 
 final locator = GetIt.I;
 
