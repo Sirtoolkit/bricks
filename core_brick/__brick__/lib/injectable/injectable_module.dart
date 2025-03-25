e80c3df2525bbac33a36b7f7853c9754ad88d6ec
Cesar Posada <cesarleonardo537@gmail.com>
2025-03-24 22:17:00 -0600

    Add lazy singleton for http.Client in RegisterModule

--- core_brick/__brick__/lib/injectable/injectable_module.dart
+++ core_brick/__brick__/lib/injectable/injectable_module.dart
@@ -1,4 +1,11 @@
 import 'package:injectable/injectable.dart';
+import 'package:http/http.dart' as http;
 
 @module
-abstract class RegisterModule {}
+abstract class RegisterModule {
+  @lazySingleton
+  http.Client get httpClient {
+    final client = http.Client();
+    return client;
+  }
+}
