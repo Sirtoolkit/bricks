65b348721486a9b27be5d0d40b72d310bd89b11b
Cesar Posada <cesarleonardo537@gmail.com>
2025-03-26 07:21:25 -0600

    Implement connectivity check using connectivity_plus package in ConnectivityServiceImpl

--- core_brick/__brick__/lib/core/services/connectivity_service.dart
+++ core_brick/__brick__/lib/core/services/connectivity_service.dart
@@ -1,19 +1,16 @@
 import 'package:injectable/injectable.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 abstract class ConnectivityService {
   Future<bool> isConnected();
 }
 
-// Una implementación básica que podrías usar o adaptar
 @LazySingleton(as: ConnectivityService)
 class ConnectivityServiceImpl implements ConnectivityService {
   @override
   Future<bool> isConnected() async {
-    // Implementación dependiendo de la librería que uses
-    // Por ejemplo, usando package:connectivity_plus:
-    // final result = await Connectivity().checkConnectivity();
-    // return result.contains(ConnectivityResult.wifi) ||
-    //     result.contains(ConnectivityResult.mobile);
-    return true;
+    final result = await Connectivity().checkConnectivity();
+    return result.contains(ConnectivityResult.wifi) ||
+        result.contains(ConnectivityResult.mobile);
   }
 }
