7b9e321daf503edc61b2be2938eef2009181beca
Cesar Posada <cesarleonardo537@gmail.com>
2025-03-30 15:17:29 -0600

    Update data.dart to replace REST repository export with GraphQL repository export

--- feature_graphql_brick/__brick__/lib/features/{{name.snakeCase()}}/data/data.dart
+++ feature_graphql_brick/__brick__/lib/features/{{name.snakeCase()}}/data/data.dart
@@ -3,7 +3,7 @@ export 'models/{{name.snakeCase()}}_model.dart';
 export 'datasources/{{name.snakeCase()}}_graphql_data_source.dart';
 export 'datasources/{{name.snakeCase()}}_local_data_source.dart';
 
-export 'repositories/{{name.snakeCase()}}_repository_rest.dart';
+export 'repositories/{{name.snakeCase()}}_repository_graphql.dart';
 
 export 'graphql/{{name.snakeCase()}}_queries.dart';
 export 'graphql/{{name.snakeCase()}}_mutations.dart';
